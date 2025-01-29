88f0fe8f648b05823c8f63a50582a659d792d994
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-01-29 16:36:08 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,6 @@
 function sum(a) {
-    if (a.length == 0) {
-        return 0; 
-    }
-    var sum = a[0];
-    for(var i = 1; i < a.length; i++) {
+    var sum = 0;
+    for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
